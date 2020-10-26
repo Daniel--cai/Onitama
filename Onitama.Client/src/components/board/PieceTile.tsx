a1309f7be81014f939fe320e3cdc215ce5449988
Daniel <daniel.cai92@gmail.com>
2020-10-27 01:47:04 +1100

    Add prettierrc and eslintrc, rearrange package json

--- Onitama.Client/src/components/board/PieceTile.tsx
+++ Onitama.Client/src/components/board/PieceTile.tsx
@@ -1,12 +1,12 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
-import * as styles from "./Board.styles";
+import { jsx } from 'theme-ui';
+import * as styles from './Board.styles';
 
-import React, { useEffect } from "react";
-import { useDrag } from "react-dnd";
-import { PieceType, Colour } from "../../store/pieces/models";
-import { useSelector } from "react-redux";
-import { State } from "../../store";
+import React, { useEffect } from 'react';
+import { useDrag } from 'react-dnd';
+import { PieceType, Colour } from '../../store/pieces/models';
+import { useSelector } from 'react-redux';
+import { State } from '../../store';
 
 export const PieceTile: React.FC<{
   type: PieceType;
@@ -15,19 +15,17 @@ export const PieceTile: React.FC<{
   onDrag: any;
 }> = (props) => {
   const type = props.type === PieceType.Master;
-  const card = useSelector<State, State["card"]>((store) => store.card);
+  const card = useSelector<State, State['card']>((store) => store.card);
 
   const [{ isDragging }, drag] = useDrag({
     item: { type: props.id.toString() },
     canDrag: card.selected !== -1,
     collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
+      isDragging: !!monitor.isDragging()
+    })
   });
   useEffect(() => {
     props.onDrag(isDragging, props.id);
   }, [isDragging]);
-  return (
-    <div sx={styles.pieceCss(props.colour, type, isDragging)} ref={drag}></div>
-  );
+  return <div sx={styles.pieceCss(props.colour, type, isDragging)} ref={drag}></div>;
 };
